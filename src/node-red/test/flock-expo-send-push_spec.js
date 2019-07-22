d2c4b40d7beb0ff13ea77daa79bd80020f9ef235
Copple <pcopplestone@gmail.com>
2019-07-22 13:05:34 +0800

    Example setup for Node-RED

---
+++ src/node-red/test/flock-expo-send-push_spec.js
@@ -0,0 +1,44 @@
+const should = require('should')
+const helper = require('node-red-node-test-helper')
+const lowerNode = require('../nodes/flock-expo-send-push.js')
+
+helper.init(require.resolve('node-red'))
+
+const TYPE = 'flock-expo-send-push'
+const NAME = 'flock-expo-send-push'
+
+describe('flock-expo-send-push Node', function() {
+  beforeEach(function(done) {
+    helper.startServer(done)
+  })
+
+  afterEach(function(done) {
+    helper.unload()
+    helper.stopServer(done)
+  })
+
+  it('should be loaded', function(done) {
+    var flow = [{ id: 'n1', type: TYPE, name: NAME }]
+    helper.load(lowerNode, flow, function() {
+      var n1 = helper.getNode('n1')
+      n1.should.have.property('name', NAME)
+      done()
+    })
+  })
+
+  it('should make payload lower case', function(done) {
+    var flow = [
+      { id: 'n1', type: TYPE, name: NAME, wires: [['n2']] },
+      { id: 'n2', type: 'helper' },
+    ]
+    helper.load(lowerNode, flow, function() {
+      var n2 = helper.getNode('n2')
+      var n1 = helper.getNode('n1')
+      n2.on('input', function(msg) {
+        msg.should.have.property('payload', 'uppercase')
+        done()
+      })
+      n1.receive({ payload: 'UpperCase' })
+    })
+  })
+})
